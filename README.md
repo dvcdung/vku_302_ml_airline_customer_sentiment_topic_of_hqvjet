946acd3e96674070b402e86f4b89cec396d09c62
dvcdung <dvcvochidung@gmail.com>
2024-05-13 08:56:23 +0700

    John updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vku_302_ml_airline_customer_sentiment_topic_of_hqvjet
+<!-- TITLE 2 >-----  --> <h3>Directory Structure</h3>
+- <img src="assets/dataset/images/0001.png" alt="image" width="500"/>
--- models/constant.py
+++ models/constant.py
@@ -2,3 +2,4 @@ DATASET_FILE_PATH = 'assets/dataset/data.csv'
 FEATURES_FILE_PATH = 'assets/dataset/PhoBERT_features_text.npy'
 
 TRAINED_MODEL_FILE_PATH = 'assets/trained_models/decision_tree_model.pkl'
+TRAINED_KNN_MODEL_FILE_PATH = 'assets/trained_models/knn_Model.pkl'
